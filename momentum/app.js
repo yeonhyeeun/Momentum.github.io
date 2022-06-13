6e1d860f14c9060c7565c39c0c42bebc80585761
YeonHyeEun <21900442@handong.edu>
2022-06-13 23:46:29 +0900

    commit msg3

--- momentum/app.js
+++ momentum/app.js
@@ -5,13 +5,17 @@ const loginForm = document.getElementById("login-from");
 const loginInput = loginForm.querySelector("input");
 const loginButton = loginForm.querySelector("button");
 
-function onLoginBtnClick() {
+
+function onLoginSubmit(event) {
+    event.preventDefault(); //브라우저의 기본 동작을 멈추게 함 ex.submit 
+
+// function onLoginBtnClick() {
     //log전에 .dir로 속성 봐서 value를 써야하는것을 알게됨. 
     // console.log(loginInput.value);
     // console.log("click!!!");
-    const username = loginInput.value;
 
     //if-else 문을 사용한 유효성 검사 
+    // const username = loginInput.value;
     // if( username === "") {
     //     alert("Please write your name");
     //     console.log("Please write your name");
@@ -20,11 +24,11 @@ function onLoginBtnClick() {
     // }
 
     //html에서 기존에 만들어진 조건을 사용하여 간략화. (required, max-length ...)
-    console.log(username);
+    console.log(loginInput.value); //콘솔에 loginInput 변수가 가지는 value값을 보여라.  
 
 }
 
+loginForm.addEventListener("submit", onLoginSubmit); 
 
 
-
-loginButton.addEventListener("click",onLoginBtnClick);
+// loginButton.addEventListener("click",onLoginBtnClick);
