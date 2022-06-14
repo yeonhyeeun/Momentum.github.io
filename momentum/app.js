7f55882bdc9bc5fced26fde195dce0443f29504b
YeonHyeEun <21900442@handong.edu>
2022-06-15 01:37:08 +0900

    edit ~4.6

--- momentum/app.js
+++ momentum/app.js
@@ -1,24 +1,40 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-// const loginForm = document.getElementById("login-from");
+const greeting = document.querySelector("#greeting");
 
-const link = document.querySelector("a"); 
-
-//document가 아니라 loginForm에서 바로 찾을 수 있음 
-// const loginInput = loginForm.querySelector("input");
-// const loginButton = loginForm.querySelector("button");
-
+//실수를 만들고 싶지않은 string 변수화 
+const HIDDEN_CLASSNAME = "hidden"; 
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
     event.preventDefault(); //브라우저의 기본 동작을 멈추게 함 ex.submit 
-    // console.log(loginInput.value); //콘솔에 loginInput 변수가 가지는 value값을 보여라.  
+    loginForm.classList.add(HIDDEN_CLASSNAME); 
     const username = loginInput.value; 
-    loginForm.classList.add("hidden"); 
-    console.log(username);    
+    // console.log(username);    
+    // greeting.innerText = "Hello " + username;
+    localStorage.setItem(USERNAME_KEY, username); 
+    
+    paintGreetings(username); 
+    // greeting.innerText = `Hello ${username}`; //윗줄과 같은 코드 
+    // //맥 기준 : ``백틱 입력은 option누르고 ~키(원화)키 누르기 
+    // greeting.classList.remove(HIDDEN_CLASSNAME); 
 }
+
+function paintGreetings(username) {
+    greeting.innerText = `Hello ${username}`; 
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+
  
-// function handleLinkClick(event) {}
-
-loginForm.addEventListener("sucbmit", onLoginSubmit); 
-
-// link.addEventListener("click",handleLinkClick); 
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+if(savedUsername === null) {
+    //show the form :비어있으니 작성할 form 주고 제출하도록 하기 
+  loginForm.classList.remove(HIDDEN_CLASSNAME); 
+  loginForm.addEventListener("submit", onLoginSubmit); 
+} else {     //새로고침해도 이름 기억할 수 있도록 하기 
+    paintGreetings(savedUsername); 
+    //show the greetings - 나타날 greeting에 이름 저장해주기 
+    // greeting.innerText = `Hello ${savedUsername}`; 
+    // greeting.classList.remove(HIDDEN_CLASSNAME); //보이기 
+}
