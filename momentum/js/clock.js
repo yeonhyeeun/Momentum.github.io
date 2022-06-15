b01bbe9e3a43e924a97c84a15748f68ccb3b6865
YeonHyeEun <21900442@handong.edu>
2022-06-15 22:26:48 +0900

    clock 수정, 초,분 00두자리수로 보이도록

--- momentum/js/clock.js
+++ momentum/js/clock.js
@@ -2,13 +2,20 @@ const clock = document.querySelector("h2#clock");
 
 function getClock() {
     const date = new Date(); 
-    clock.innerText = (`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`)
-}
+    //string으로 형변환해야 padstart 가능 
+    const hours = String(date.getHours()).padStart(2,"0"); 
+    const minutes = String(date.getMinutes()).padStart(2,"0"); 
+    const seconds = String(date.getSeconds()).padStart(2,"0");  
 
+    clock.innerText = (`${hours}:${minutes}:${seconds}`);
+} 
 //setInterval 함수 1인자:실행하고싶은 함수, 2인자:몇초간격? 5000=5초 
 // setInterval(sayHello, 5000); 
 
 //setTimeout 위와 비슷하게 사용, 한번만 보여줌 
 // setTimeout(sayHello,5000);
-getClock(); 
+
+getClock(); //바로 보기 위해 미리 작동 
 setInterval(getClock, 1000);
+
+//padStart()-앞에 추가, padEnd()-뒤에 추가 배움 
