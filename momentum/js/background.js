38d5b81b291516e8e6b967f089367f8c78e134a9
YeonHyeEun <21900442@handong.edu>
2022-06-16 13:33:14 +0900

    주석 설명 추가

--- momentum/js/background.js
+++ momentum/js/background.js
@@ -6,8 +6,11 @@ const images = [
 
 const chosenImages = images[Math.floor(Math.random() * images.length)]; 
 
+//createElement라는 js에서 html요소를 사용할 수 있도록 만드는 키워드 
 const bgImage = document.createElement("img"); 
 
+//백틱 사용하는것 잊지말기. 주의. html에서 img src 하는것과 비슷하게 코드 작성 
 bgImage.src=`img/${chosenImages}`; 
 
+//append는 가장 뒤에, prepand는 앞에 나오게 함 
 document.body.appendChild(bgImage);
