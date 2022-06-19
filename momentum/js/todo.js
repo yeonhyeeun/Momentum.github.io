7e1bd4c1ef21a78187e8cf79c9296d25279640e3
YeonHyeEun <21900442@handong.edu>
2022-06-20 03:18:30 +0900

    create deleteToDo function

--- momentum/js/todo.js
+++ momentum/js/todo.js
@@ -3,15 +3,27 @@ const toDoInput = toDoForm.querySelector("input");
 //위 코드와 동일   = document.quertSelector("#todo-form input")
 const toDoList = document.getElementById("todo-list"); 
 
+function deleteToDo() {
+    
+}
+
+
+
+
 
 function paintToDo(newTodo) {
     // console.log("i will paint", newTodo);  //인자 잘 받는지 확인 
     //Js에서 html 만들기 아래 코드 
     const li = document.createElement("li"); //createElement의 변수는 html 태그여야한다. 
     const span = document.createElement("span"); 
-    li.appendChild(span); //리스트 밖에서 만들어진 span을 li안으로 넣기 
     span.innerText = newTodo; //스판에 항목의 텍스트를 넣어주기 
-    // console.log(li); 
+
+    const button = document.createElement("button"); 
+    button.innerText = "❌"; 
+    button.addEventListener("click",deleteToDo); 
+    //appent는 맨 마지막에 나와야해! 
+    li.appendChild(span); //리스트 밖에서 만들어진 span을 li안으로 넣기 
+    li.appendChild(button); 
     toDoList.appendChild(li); //미리 만들어둔 html리스트 자리에 Js로 만든 리스트를 넣어주기 super coooool
     
 }
