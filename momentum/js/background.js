1d55cb64d01828967164477a489b43b9387862a8
YeonHyeEun <21900442@handong.edu>
2022-06-20 05:08:44 +0900

    edit

--- momentum/js/background.js
+++ momentum/js/background.js
@@ -1,7 +1,9 @@
 const images = [
-    "T1.jpeg",
-    "T2.jpeg",
-    "S1.jpeg"
+    // "T1.jpeg",
+    // "T2.jpeg",
+    // "S1.jpeg"
+    "1.jpeg", "2.jpeg", "3.jpeg", "4.jpeg", "5.jpeg", "6.jpeg", "7.jpeg", "8.jpeg", "9.jpeg",
+    "10.jpeg", "11.jpeg", "12.jpeg", "13.jpeg", "14.jpeg", "15.jpeg", "16.jpeg", "17.jpeg", "18.jpeg","19.jpeg"
 ]; 
 
 const chosenImages = images[Math.floor(Math.random() * images.length)]; 
